1d8c21418711d6b30acbd6e5f5725853daaa323b
licy-123 <licy292556@gmail.com>
2025-04-21 22:40:54 +0800

    将子弹存储到编组中并实现开火功能

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,8 @@ from settings import Settings
 
 from ship import Ship
 
+from bullet import Bullet
+
 
 class AlienInvasion:
     """
@@ -24,6 +26,8 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption('Alien Invasion')
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+
 
     def run_game(self):
         """
@@ -33,6 +37,7 @@ class AlienInvasion:
             # 侦听键盘和鼠标事件
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -54,6 +59,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_ESCAPE:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """响应释放"""
@@ -62,10 +69,17 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """创建一颗子弹，并将其加入编组bullets"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
         # 每次循环时都重绘屏幕
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
 
         # 让最近绘制的屏幕可见
